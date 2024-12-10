bef5e1cdb4601aaaa1f38c41a9c0bc5ed090216a
abicontreras27 <wacr.27@gmail.com>
2024-12-10 10:38:38 -0500

    creando modulo de interests

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ReportsModule } from './reports/reports.module';
 import { PrivacyModule } from './privacy/privacy.module';
+import { MessagesModule } from './messages/messages.module';
+import { InterestsModule } from './interests/interests.module';
 
 @Module({
-  imports: [ReportsModule, PrivacyModule],
+  imports: [ReportsModule, PrivacyModule, MessagesModule, InterestsModule],
   controllers: [AppController],
   providers: [AppService],
 })
