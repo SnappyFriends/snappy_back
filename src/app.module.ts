f9b56c7b6ad98dc434701cf6511f6276ffad8519
JuanMatiasGema12 <166863870+JuanMatiasGema12@users.noreply.github.com>
2024-12-10 12:20:06 -0300

    creating module/Reactions

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
+import { ReactionsModule } from './reactions/reactions.module';
 
 @Module({
-  imports: [UsersModule, PostsModule],
+  imports: [UsersModule, PostsModule, ReactionsModule],
   controllers: [AppController],
   providers: [AppService],
 })
