72309a529ed3b3576446d091702a89119e733cfe
JuanMatiasGema12 <166863870+JuanMatiasGema12@users.noreply.github.com>
2024-12-10 12:25:46 -0300

    creating module/Polls

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
 import { ReactionsModule } from './reactions/reactions.module';
 import { CommentsModule } from './comments/comments.module';
+import { PollsModule } from './polls/polls.module';
 
 @Module({
-  imports: [UsersModule, PostsModule, ReactionsModule, CommentsModule],
+  imports: [UsersModule, PostsModule, ReactionsModule, CommentsModule, PollsModule],
   controllers: [AppController],
   providers: [AppService],
 })
