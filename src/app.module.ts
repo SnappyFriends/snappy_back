e286faf2d584925ccc01e0c3111c2b59e174fb39
abicontreras27 <wacr.27@gmail.com>
2024-12-10 10:27:30 -0500

    creando modulo de privacy

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ReportsModule } from './reports/reports.module';
+import { PrivacyModule } from './privacy/privacy.module';
 
 @Module({
-  imports: [ReportsModule],
+  imports: [ReportsModule, PrivacyModule],
   controllers: [AppController],
   providers: [AppService],
 })
