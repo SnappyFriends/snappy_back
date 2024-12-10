087ed54555b9fed5179743c4face22590bc85807
JuanMatiasGema12 <166863870+JuanMatiasGema12@users.noreply.github.com>
2024-12-10 17:57:18 -0300

    adding dependencies

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -9,15 +9,15 @@ export class PostsService {
   }
 
   findAll() {
-    return `This action returns all posts`;
+    return `This action returns all post`;
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} posts`;
+    return `This action returns a #${id} post`;
   }
 
   update(id: number, updatePostDto: UpdatePostDto) {
-    return `This action updates a #${id} posts`;
+    return `This action updates a #${id} post`;
   }
 
   remove(id: number) {
