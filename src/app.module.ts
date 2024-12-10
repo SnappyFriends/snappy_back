404de9db976a41da48fe3a709b19731b0d0b101f
JuanMatiasGema12 <166863870+JuanMatiasGema12@users.noreply.github.com>
2024-12-10 12:48:23 -0300

    Reverting changes in main branch

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { PostsModule } from './posts/posts.module';
-import { ReactionsModule } from './reactions/reactions.module';
-import { CommentsModule } from './comments/comments.module';
-import { PollsModule } from './polls/polls.module';
 
 @Module({
-  imports: [UsersModule, PostsModule, ReactionsModule, CommentsModule, PollsModule],
+  imports: [UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
