743c4db3847b2a54cf5fe8c76addadf15682b382
abicontreras27 <wacr.27@gmail.com>
2024-12-10 12:44:42 -0500

    Revert "Resolviendo conflictos parte 3, organizando file app.module"
    This reverts commit f0629355ddeb6a5da353df59113cbe08a38b8719, reversing
    changes made to bcffbd906f714a4f094fb18011db8bb090719610.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,38 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+<<<<<<< HEAD
+<<<<<<< HEAD
 import { ReportsModule } from './reports/reports.module';
 import { PrivacyModule } from './privacy/privacy.module';
 import { MessagesModule } from './messages/messages.module';
 import { InterestsModule } from './interests/interests.module';
 import { FriendshipsModule } from './friendships/friendships.module';
 
+
 @Module({
   imports: [ReportsModule, PrivacyModule, MessagesModule, InterestsModule, FriendshipsModule],
+=======
+import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
+import { ReactionsModule } from './reactions/reactions.module';
+import { CommentsModule } from './comments/comments.module';
+import { PollsModule } from './polls/polls.module';
+import { FriendshipsModule } from './friendships/friendships.module';
+
+@Module({
+  imports: [UsersModule, PostsModule, ReactionsModule, CommentsModule, PollsModule],
+>>>>>>> a546591e970a41e039341977fcbe024d9653d625
+=======
+import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
+import { ReactionsModule } from './reactions/reactions.module';
+import { CommentsModule } from './comments/comments.module';
+import { PollsModule } from './polls/polls.module';
+
+@Module({
+  imports: [UsersModule, PostsModule, ReactionsModule, CommentsModule, PollsModule],
+>>>>>>> a546591e970a41e039341977fcbe024d9653d625
   controllers: [AppController],
   providers: [AppService],
 })
