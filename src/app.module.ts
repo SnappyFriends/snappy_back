325074531bebce29938bbe65dc02482909fd22d5
JuanMatiasGema12 <166863870+JuanMatiasGema12@users.noreply.github.com>
2024-12-10 18:14:26 -0300

    holaaa

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,9 +17,7 @@ import { MessagesModule } from './messages/messages.module';
 import { InterestsModule } from './interests/interests.module';
 import { FriendshipsModule } from './friendships/friendships.module';
 
-
 @Module({
-<<<<<<< HEAD
   imports: [
     ConfigModule.forRoot({ isGlobal: true, load: [typeorm] }),
     TypeOrmModule.forRootAsync({
@@ -35,16 +33,9 @@ import { FriendshipsModule } from './friendships/friendships.module';
     ReportsModule,
     PrivacyModule,
     InterestsModule,
+    FriendshipsModule,
     MessagesModule,
   ],
-=======
-  imports: [ConfigModule.forRoot({ isGlobal: true, load: [typeorm] }),
-  TypeOrmModule.forRootAsync({
-    inject: [ConfigService],
-    useFactory: (configTypeORM: ConfigService) => configTypeORM.get('typeorm')
-  }),
-    UsersModule, PostsModule, ReactionsModule, CommentsModule, PollsModule, ReportsModule, PrivacyModule, InterestsModule, FriendshipsModule],
->>>>>>> d4a98499fabdffc4a7f5367044802f87f4cd76f9
   controllers: [AppController],
   providers: [AppService],
 })
