699e8f2a43b16f3bfe27a9abc6ab60987ddb71f7
JuanMatiasGema12 <166863870+JuanMatiasGema12@users.noreply.github.com>
2024-12-11 14:22:42 -0300

    Adding Friendships entity relations to Users

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,64 +1,71 @@
-import { Stories } from "src/stories/entities/stories.entity";
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
-import { v4 as uuid } from "uuid";
+import { Friendship } from 'src/friendships/entities/friendship.entity';
+import { Stories } from 'src/stories/entities/stories.entity';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { v4 as uuid } from 'uuid';
 
 @Entity({
-    name: 'Users'
+  name: 'Users',
 })
 export class User {
-    @PrimaryGeneratedColumn('uuid')
-    id: string = uuid();
+  @PrimaryGeneratedColumn('uuid')
+  id: string = uuid();
 
-    @Column({
-        nullable: false
-    })
-    fullname: string;
+  @Column({
+    nullable: false,
+  })
+  fullname: string;
 
-    @Column({
-        nullable: false
-    })
-    username: string
+  @Column({
+    nullable: false,
+  })
+  username: string;
 
-    @Column({
-        nullable: false
-    })
-    email: string
+  @Column({
+    nullable: false,
+  })
+  email: string;
 
-    @Column({
-        nullable: false
-    })
-    password: string
+  @Column({
+    nullable: false,
+  })
+  password: string;
 
-    @Column({
-        nullable: false
-    })
-    registration_date: Date
+  @Column({
+    nullable: false,
+  })
+  registration_date: Date;
 
-    @Column({
-        nullable: false
-    })
-    last_login_date: Date
+  @Column({
+    nullable: false,
+  })
+  last_login_date: Date;
 
-    @Column({
-        nullable: false
-    })
-    user_type: string
+  @Column({
+    nullable: false,
+  })
+  user_type: string;
 
-    @Column({
-        nullable: false
-    })
-    status: string
+  @Column({
+    nullable: false,
+  })
+  status: string;
 
-    @Column({
-        nullable: false
-    })
-    profile_image: string
+  @Column({
+    nullable: false,
+  })
+  profile_image: string;
 
-    @Column({
-        nullable: false
-    })
-    location: string
+  @Column({
+    nullable: false,
+  })
+  location: string;
 
-    @OneToMany(() => Stories, (story) => story.user_id)
-    stories: Stories[]
+  @OneToMany(() => Stories, (story) => story.user_id)
+  stories: Stories[];
+
+  @OneToMany(() => Friendship, (friendship) => friendship.user1)
+  friendships1: Friendship[];
+
+  @OneToMany(() => Friendship, (friendship) => friendship.user2)
+  friendships2: Friendship[];
 }
