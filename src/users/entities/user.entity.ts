acc1bb9593ae73078afd9a721e899e3ce583a316
JuanMatiasGema12 <166863870+JuanMatiasGema12@users.noreply.github.com>
2024-12-11 14:12:49 -0300

    Adding Privacy entity to Users

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,60 +1,64 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
-import { v4 as uuid } from "uuid";
+import { Privacy } from 'src/privacy/entities/privacy.entity';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { v4 as uuid } from 'uuid';
 
 @Entity({
-    name: 'Users'
+  name: 'Users',
 })
 export class User {
-    @PrimaryGeneratedColumn('uuid')
-    id: string = uuid();
+  @PrimaryGeneratedColumn('uuid')
+  id: string = uuid();
 
-    @Column({
-        nullable: false
-    })
-    fullname: string;
+  @Column({
+    nullable: false,
+  })
+  fullname: string;
 
-    @Column({
-        nullable: false
-    })
-    username: string
+  @Column({
+    nullable: false,
+  })
+  username: string;
 
-    @Column({
-        nullable: false
-    })
-    email: string
+  @Column({
+    nullable: false,
+  })
+  email: string;
 
-    @Column({
-        nullable: false
-    })
-    password: string
+  @Column({
+    nullable: false,
+  })
+  password: string;
 
-    @Column({
-        nullable: false
-    })
-    registration_date: Date
+  @Column({
+    nullable: false,
+  })
+  registration_date: Date;
 
-    @Column({
-        nullable: false
-    })
-    last_login_date: Date
+  @Column({
+    nullable: false,
+  })
+  last_login_date: Date;
 
-    @Column({
-        nullable: false
-    })
-    user_type: string
+  @Column({
+    nullable: false,
+  })
+  user_type: string;
 
-    @Column({
-        nullable: false
-    })
-    status: string
+  @Column({
+    nullable: false,
+  })
+  status: string;
 
-    @Column({
-        nullable: false
-    })
-    profile_image: string
+  @Column({
+    nullable: false,
+  })
+  profile_image: string;
 
-    @Column({
-        nullable: false
-    })
-    location: string
+  @Column({
+    nullable: false,
+  })
+  location: string;
+
+  @OneToMany(() => Privacy, (privacy) => privacy.user)
+  privacy: Privacy[];
 }
