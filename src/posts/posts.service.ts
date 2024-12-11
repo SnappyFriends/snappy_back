6499213c72b2df08c8450e4f6c09681d829ee8cf
JuanMatiasGema12 <166863870+JuanMatiasGema12@users.noreply.github.com>
2024-12-11 17:25:06 -0300

    Creating /post a module_Posts

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,11 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { UpdatePostDto } from './dto/update-post-dto';
 import { CreatePostDto } from './dto/create-post-dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Post } from './entities/post.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class PostsService {
-  create(createPostDto: CreatePostDto) {
-    return 'This action adds a new posts';
+  constructor(
+    @InjectRepository(Post) private postsRepository: Repository<Post>,
+  ) {}
+
+  async create(createPostDto: CreatePostDto) {
+    try {
+      const newPost = this.postsRepository.create(createPostDto);
+
+      console.log('Post creado.');
+      return await this.postsRepository.save(newPost);
+    } catch {
+      throw new BadRequestException(
+        'Ocurrió un error inesperado al crear el post. Inténtelo de nuevo. ',
+      );
+    }
   }
 
   findAll() {
