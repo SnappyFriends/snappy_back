10e6baa37a49f59511b99a4aaa4b325399e86f00
JuanMatiasGema12 <166863870+JuanMatiasGema12@users.noreply.github.com>
2024-12-11 10:18:21 -0300

    Creating Reactions Entity

--- src/reactions/entities/reaction.entity.ts
+++ src/reactions/entities/reaction.entity.ts
@@ -1,1 +1,39 @@
-export class Reaction {}
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  Column,
+  CreateDateColumn,
+  Index,
+} from 'typeorm';
+import { Post } from '../../posts/entities/post.entity';
+import { User } from '../../users/entities/user.entity';
+
+@Entity('reactions')
+@Index(['user', 'post'], { unique: true })
+export class Reaction {
+  @PrimaryGeneratedColumn('uuid')
+  reaction_id: string;
+
+  @Column({
+    type: 'enum',
+    enum: ['like', 'dislike'],
+    nullable: false,
+  })
+  reaction_type: 'like' | 'dislike';
+
+  @CreateDateColumn()
+  reaction_date: Date;
+
+  @ManyToOne(() => Post, (post) => post.reactions, {
+    nullable: false,
+    onDelete: 'CASCADE',
+  })
+  post: Post;
+
+  @ManyToOne(() => User, (user) => user.reactions, {
+    nullable: false,
+    onDelete: 'CASCADE',
+  })
+  user: User;
+}
