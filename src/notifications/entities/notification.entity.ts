4c6abdacebc296f33bb0b73b15f1757c8ca78b62
JuanMatiasGema12 <166863870+JuanMatiasGema12@users.noreply.github.com>
2024-12-17 00:18:22 -0300

    Creación de modulo y metodos CRUD para Notifications. Finalizado

---
+++ src/notifications/entities/notification.entity.ts
@@ -0,0 +1,54 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  JoinColumn,
+  CreateDateColumn,
+} from 'typeorm';
+import { v4 as uuid } from 'uuid';
+import { User } from 'src/users/entities/user.entity'; // Asegúrate de importar la entidad User
+
+export enum NotificationType {
+  FRIEND_REQUEST = 'friend_request',
+  MESSAGE = 'message',
+  POST_REACTION = 'post_reaction',
+  COMMENT = 'comment',
+  GROUP_INVITATION = 'group_invitation',
+  SYSTEM = 'system',
+  PURCHASE = 'purchase',
+}
+
+export enum NotificationStatus {
+  READ = 'read',
+  UNREAD = 'unread',
+}
+
+@Entity('notifications')
+export class Notification {
+  @PrimaryGeneratedColumn('uuid')
+  notification_id: string = uuid();
+
+  @Column({ type: 'text' })
+  content: string;
+
+  @Column({
+    type: 'enum',
+    enum: NotificationType,
+  })
+  type: NotificationType;
+
+  @Column({
+    type: 'enum',
+    enum: NotificationStatus,
+    default: NotificationStatus.UNREAD,
+  })
+  status: NotificationStatus;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  creation_date: Date;
+
+  @ManyToOne(() => User, (user) => user.notifications)
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+}
