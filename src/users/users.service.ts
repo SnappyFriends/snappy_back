0025beb29252e0592e03b97518c74c970c5d27ed
JuanMatiasGema12 <166863870+JuanMatiasGema12@users.noreply.github.com>
2024-12-20 10:20:53 -0300

    Solucionado el problema de los filtros

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,35 +4,47 @@ import { GetUsersFiltersDTO, UpdateUserDTO } from './dto/user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { In, Repository } from 'typeorm';
-import * as bcrypt from "bcrypt";
+import * as bcrypt from 'bcrypt';
 import { Interest } from 'src/interests/entities/interests.entity';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User) private usersRepository: Repository<User>,
-    @InjectRepository(Interest) private interestsRepository: Repository<Interest>
-  ) { }
+    @InjectRepository(Interest)
+    private interestsRepository: Repository<Interest>,
+  ) {}
 
   async getUsers(filters: GetUsersFiltersDTO) {
     const { page = 1, limit = 5, interests } = filters;
 
-    const where: any = {};
+    const queryBuilder = this.usersRepository
+      .createQueryBuilder('user')
+      .leftJoinAndSelect('user.interests', 'interest')
+      .skip((page - 1) * limit)
+      .take(limit);
 
-    if (interests && interests.length > 0) {
-      where.interests = {
-        name: In(interests), 
-      };
+    // Asegurarnos de que 'interests' es siempre un arreglo, incluso si solo se pasa un valor
+    if (interests && Array.isArray(interests) && interests.length > 0) {
+      queryBuilder.andWhere('interest.name IN (:...interests)', { interests });
+    } else if (interests && !Array.isArray(interests)) {
+      // Si interests es un solo valor, lo convertimos en un arreglo
+      queryBuilder.andWhere('interest.name IN (:...interests)', {
+        interests: [interests],
+      });
     }
 
-    const [usersFound] = await this.usersRepository.findAndCount({
-      where,
-      skip: (page - 1) * limit,
-      take: limit,
-      relations: ['stories', 'interests', 'privacy', 'responses', 'reportedReports', 'reportingReports', 'polls', 'posts', 'reactions', 'comments', 'groupMembers']
-    });
+    const usersFound = await queryBuilder.getMany();
 
-    const usersWithoutPassword = usersFound.map(({ password, ...userWithoutPassword }) => userWithoutPassword);
+    // Si no se encuentran usuarios, devolver un array vacío
+    if (!usersFound || usersFound.length === 0) {
+      console.log('No users found');
+      return [];
+    }
+
+    const usersWithoutPassword = usersFound.map(
+      ({ password, ...userWithoutPassword }) => userWithoutPassword,
+    );
 
     return usersWithoutPassword;
   }
@@ -40,10 +52,23 @@ export class UsersService {
   async getUserById(id: string) {
     const userFound = await this.usersRepository.findOne({
       where: { id },
-      relations: ['stories', 'interests', 'privacy', 'responses', 'reportedReports', 'reportingReports', 'polls', 'posts', 'reactions', 'comments', 'groupMembers']
-    })
+      relations: [
+        'stories',
+        'interests',
+        'privacy',
+        'responses',
+        'reportedReports',
+        'reportingReports',
+        'polls',
+        'posts',
+        'reactions',
+        'comments',
+        'groupMembers',
+      ],
+    });
 
-    if (!userFound) throw new NotFoundException(`No se encontró un usuario con el ID ${id}`);
+    if (!userFound)
+      throw new NotFoundException(`No se encontró un usuario con el ID ${id}`);
 
     const { password, ...userWithoutPassword } = userFound;
     return userWithoutPassword;
@@ -53,13 +78,24 @@ export class UsersService {
     const userFound = await this.usersRepository.findOne({
       where: { username },
       relations: [
-        'stories', 'interests', 'privacy', 'responses', 'reportedReports',
-        'reportingReports', 'polls', 'posts', 'reactions', 'comments',
-        'groupMembers'
-      ]
+        'stories',
+        'interests',
+        'privacy',
+        'responses',
+        'reportedReports',
+        'reportingReports',
+        'polls',
+        'posts',
+        'reactions',
+        'comments',
+        'groupMembers',
+      ],
     });
 
-    if (!userFound) throw new NotFoundException(`No se encontró un usuario con el username ${username}`);
+    if (!userFound)
+      throw new NotFoundException(
+        `No se encontró un usuario con el username ${username}`,
+      );
 
     const { password, ...userWithoutPassword } = userFound;
     return userWithoutPassword;
@@ -71,9 +107,10 @@ export class UsersService {
     }
 
     const result = await this.usersRepository.update(id, userData);
-    if (result.affected === 0) throw new NotFoundException(`No se encontró un usuario con el ID ${id}`);
+    if (result.affected === 0)
+      throw new NotFoundException(`No se encontró un usuario con el ID ${id}`);
 
-    const updatedUser = await this.usersRepository.findOne({ where: { id } })
+    const updatedUser = await this.usersRepository.findOne({ where: { id } });
 
     const { password, ...userWithoutPassword } = updatedUser;
 
@@ -82,23 +119,37 @@ export class UsersService {
 
   async deleteUser(id: string) {
     const result = await this.usersRepository.delete(id);
-    if (result.affected === 0) throw new NotFoundException(`No se encontró un usuario con el ID ${id}`);
+    if (result.affected === 0)
+      throw new NotFoundException(`No se encontró un usuario con el ID ${id}`);
 
-    return "Usuario eliminado correctamente.";
+    return 'Usuario eliminado correctamente.';
   }
 
   async assignInterestToUser(userId: string, interestId: string) {
-    const user = await this.usersRepository.findOne({ where: { id: userId }, relations: ['interests'] });
+    const user = await this.usersRepository.findOne({
+      where: { id: userId },
+      relations: ['interests'],
+    });
     if (!user) {
-      throw new NotFoundException(`No se encontró el usuario con el ID ${userId}`);
+      throw new NotFoundException(
+        `No se encontró el usuario con el ID ${userId}`,
+      );
     }
 
-    const interest = await this.interestsRepository.findOne({ where: { interest_id: interestId } });
+    const interest = await this.interestsRepository.findOne({
+      where: { interest_id: interestId },
+    });
     if (!interest) {
-      throw new NotFoundException(`No se encontró el interés con el ID ${interestId}`);
+      throw new NotFoundException(
+        `No se encontró el interés con el ID ${interestId}`,
+      );
     }
 
-    if (user.interests.some((existingInterest) => existingInterest.interest_id === interestId)) {
+    if (
+      user.interests.some(
+        (existingInterest) => existingInterest.interest_id === interestId,
+      )
+    ) {
       throw new NotFoundException(`El usuario ya tiene este interés`);
     }
 
@@ -108,7 +159,7 @@ export class UsersService {
     return {
       message: 'Interés asignado exitosamente',
       usuario: user.username,
-      interests: user.interests
+      interests: user.interests,
     };
   }
 }
