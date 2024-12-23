c1479c9c5500dd2f8ed8db25c5648ee1b6e99093
abicontreras27 <wacr.27@gmail.com>
2024-12-23 12:34:28 -0500

    modificando import partialType en swagger para funcionar UpdateCommentDto

--- src/comments/dto/update-comment.dto.ts
+++ src/comments/dto/update-comment.dto.ts
@@ -1,5 +1,5 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateCommentDto } from './create-comment.dto';
+import { PartialType } from '@nestjs/swagger';
+import { CreateCommentDto, } from './create-comment.dto';
 
 
 export class UpdateCommentDto extends PartialType(CreateCommentDto) { }
