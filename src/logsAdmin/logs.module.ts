92009ce1328e5b6b96ed98dd5254765b013a797a
abicontreras27 <wacr.27@gmail.com>
2025-01-07 23:42:25 -0500

    creando estadisticas de reports

--- src/logsAdmin/logs.module.ts
+++ src/logsAdmin/logs.module.ts
@@ -4,10 +4,11 @@ import { AdminController } from './logs.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Log } from './entities/logs.entity';
 import { User } from 'src/users/entities/user.entity';
+import { Report } from 'src/reports/entities/report.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Log, User])],
+  imports: [TypeOrmModule.forFeature([Log, User, Report])],
   controllers: [AdminController],
-  providers: [LogsService],
+  providers: [LogsService,],
 })
 export class LogsModule { }
