515ef0a45624eeb0d4971fd317826f4167b8b307
Artifexted <artifexted@gmail.com>
2025-01-09 21:13:39 -0300

    Retorno de userSender en los get notifitations

--- src/notifications/notifications.service.ts
+++ src/notifications/notifications.service.ts
@@ -54,7 +54,7 @@ export class NotificationsService {
   async findAll() {
     try {
       const notifications = await this.notificationRepository.find({
-        relations: ['user'],
+        relations: ['user', 'user_sender'],
       });
 
       const responseObject = notifications.map((notification) => ({
@@ -75,7 +75,7 @@ export class NotificationsService {
     try {
       const notifications = await this.notificationRepository.findOne({
         where: { notification_id: id },
-        relations: ['user'],
+        relations: ['user', 'user_sender'],
       });
       if (!notifications) {
         throw new NotFoundException('Notificación no encontrada.');
@@ -96,7 +96,7 @@ export class NotificationsService {
   }
 
   async findByUser(id: string) {
-    const userFound = await this.usersRepository.findOne({ where: { id } });
+    const userFound = await this.usersRepository.findOne({ where: { id }, relations: ['user', 'user_sender'] });
     if (!userFound) throw new NotFoundException(`Usuario con id ${id} no encontrado.`);
 
     const notifications = await this.notificationRepository.find({
